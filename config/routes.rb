25641287021ebe79107e726ac6a5feb026e6a420
jvalbuena <julian.valbuena@gmail.com>
2013-04-30 15:22:56 +1000

    Setting root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 QuickBlog::Application.routes.draw do
+  root :to => 'posts#index'
   resources :posts do
-    resources :comments, :only => [:create]
-  end
-
+      resources :comments, :only => [:create]
+    end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
